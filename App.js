1c71dfa24c8871392b9e1d7b0f3af871f038b986
Silikhe <silaswans@gmail.com>
2021-02-23 03:31:02 +0300

    splashcreean and Signup

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import RootStackScreen from "./app/screens/RootStackScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootStackScreen>
+      <View style={styles.container}>
+        <Text>Silikhe</Text>
+        <StatusBar style="auto" />
+      </View>
+    </RootStackScreen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
